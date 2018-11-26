4f274fe1fe8f63cca1dd48f000065f2ab1304be1
Ramona Saintandre <thenewmona@gmail.com>
2018-11-26 10:31:38 -0500

    got components set up and router configured

--- notes.md
+++ notes.md
@@ -9,4 +9,27 @@ following along with  this walk through
 working on breaking down my components 
 got through section 1 and 2 of udactiy starting the project along with finishing up section 3 REACT portion 
 
+11/26/2018
 
+created AppNotes.js - so that I can make the components from the file, but keep the app.js in tacked
+keep losing my placed 
+//Following Roderick's walkthrough https://www.youtube.com/watch?v=5J6fs_BlVC0
+Got through 3 lessons from Tyler, created a node server , and getting a better understanding of webpack 
+
+[All Supported HTML Attributes](https://reactjs.org/docs/dom-elements.html#all-supported-html-attributes)
+
+[Rendering Elements](https://reactjs.org/docs/rendering-elements.html)
+ 
+ Setup my components finally now I am getting this error message 
+
+ `./src/App.js
+Module not found: Can't resolve 'react-router-dom' in 'C:\Users\thene\Google Drive\GWG phase 2\GWGprojects\reactnd-project-myreads-starter\src'
+This error occurred during the build time and cannot be dismissed.`
+
+[react-router-dom](https://www.npmjs.com/package/react-router-dom)
+
+got rid of the router error now need to fix this error 
+
+./src/App.js
+Module not found: `C:\Users\thene\Google Drive\GWG phase 2\GWGprojects\reactnd-project-myreads-starter\src\components\MainPage.js` does not match the corresponding path on disk `mainpage.js`.
+This error occurred during the build time and cannot be dismissed.
--- src/App.js
+++ src/App.js
@@ -1,69 +1,51 @@
-import React from 'react'
 // import * as BooksAPI from './BooksAPI'
+import React from 'react'
+import * as BooksAPI from './BooksAPI'
 import './App.css'
+import {Route} from 'react-router-dom'
+import MainPage from './components/MainPage';
+import SearchPage from './components/SearchPage';
+
+//?bookshelf.js and books.js not sure if i want to create 3 different components want to read, read, currently reading
 
 class BooksApp extends React.Component {
-  state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false
+  state = {books:[]} 
+
+  componentDidMount() {
+    BooksAPI.getAll()
+    .then(books=> {
+      this.setState({books});
+    })
+  }
+
+  updateBookshelf = (book,shelf) => {
+    BooksAPI.update(book,shelf)
+    .then(response=> {
+      book.shelf=shelf;
+      this.setState(state=>({
+        books: state.books.filter(Book=>Book.id !== book.id).concat(book)
+      }));
+    });
   }
 
   render() {
-    return (
-      <div className="app">
-        {this.state.showSearchPage ? (
-
-          //search component 
-          <div className="search-books">
-            <div className="search-books-bar">
-              <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
-              <div className="search-books-input-wrapper">
-                {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
-                <input type="text" placeholder="Search by title or author"/>
-
-              </div>
-            </div>
-            <div className="search-books-results">
-              <ol className="books-grid"></ol>
-            </div>
-          </div>
-
-
-        ) : (
-          <div className="list-books">
-
-          {/*HEADER*/}
-            <div className="list-books-title">
-              <h1>MyReads</h1>
-            </div>
-            
-{/*<Shelves/>*/}
-            
-
-              </div>
-            </div>
-
-            {/*SEARCH BUTTON*/}
-            <div className="open-search">
-              <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
-            </div>
-          </div>
-        )}
+   return (
+      <div>
+        <Route exact path="/" render={() => (
+          <MainPage
+            books={this.state.books} 
+        	updateBookshelf={this.updateBookshelf}
+          />
+        )}/>
+        <Route exact path="/search" render={() => (
+          <SearchPage
+            books={this.state.books} 
+        	updateBookshelf={this.updateBookshelf}
+          />
+        )}/>
       </div>
-    )
+    );
   }
 }
 
-export default BooksApp
+export default BooksApp;
